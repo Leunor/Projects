c58a93b580c19d059561522cc6044526e835a32c
Leunor <leunor@outlook.com>
2020-12-13 18:11:33 +0000

    Update Roller_Coaster_Project.py

--- Roller_Coaster_Project/Roller_Coaster_Project.py
+++ Roller_Coaster_Project/Roller_Coaster_Project.py
@@ -28,15 +28,26 @@ roller_coaster_ranking("El Toro", "Six Flags Great Adventure", wood)
 plt.clf()
 
 # write function to plot rankings over time for 2 roller coasters here:
-
-
-
-
-
-
-
-
-
+def two_roller_coaster_ranking(name1, park1, name2, park2, data):
+  find_rank1 = data[(data["Name"] == name1) & (data["Park"] == park1)]
+  find_rank2 = data[(data["Name"] == name2) & (data["Park"] == park2)]
+  x_axis1 = find_rank1["Year of Rank"]
+  y_axis1 = find_rank1["Rank"]
+  x_axis2 = find_rank2["Year of Rank"]
+  y_axis2 = find_rank2["Rank"]
+  ax = plt.subplot()
+  plt.plot(x_axis1, y_axis1, marker = "o", label = name1)
+  plt.plot(x_axis2, y_axis2, marker = "x", label = name2)
+  plt.xlabel("Year")
+  plt.ylabel("Rank")
+  plt.title( str(name1) + " vs " + str(name2) + " Rankings")
+  ax.set_xticks(x_axis1.values)
+  ax.set_yticks(pd.concat([y_axis1, y_axis2]).drop_duplicates().values)
+  ax.set_yticklabels(pd.concat([y_axis1, y_axis2]).drop_duplicates().values)
+  ax.invert_yaxis()
+  plt.legend()
+  return plt.show()
+two_roller_coaster_ranking("El Toro", "Six Flags Great Adventure", "Boulder Dash", "Lake Compounce", wood)
 
 plt.clf()
 
